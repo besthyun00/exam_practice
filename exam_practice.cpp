f03cd4e70927bef6ec2ef127de403ebf6c5654a6
besthyun00 <ppjor@gmail.com>
2023-04-18 20:54:37 +0900

    message

--- exam_practice.cpp
+++ exam_practice.cpp
@@ -11,7 +11,7 @@ int ArrayMax(int score[], int n) {
 }
 
 int main(void) {
-	int score[6] = { 27,35,66,45,34,65 };
+	int score[6] = { 27,35,67,45,34,65 };
 	int bestScore = ArrayMax(score, 6);
 	printf("최고 점수는 %d점 입니다.", bestScore);
 }
